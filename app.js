0600c9c0690a35dc3821adc8b5ab78a6602ca6e8
Jcampsala <jcampsala@gmail.com>
2021-02-27 15:52:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+const Discord = require('discord.js');
+require('dotenv').config();
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Bot is ready');
+});
+
+client.on('message', () => {
+    if (msg.content === 'Hello') msg.reply('Hi');
+});
+
+client.login(process.env.BOT_TOKEN);
